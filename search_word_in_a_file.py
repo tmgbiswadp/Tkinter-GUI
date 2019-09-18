215f1926fa601fe1eb042c0b5ed375de34080b08
TAMANG <harsha2019>
2019-09-18 22:08:17 +0545

    GUI for searching a word in word in a text file with multiple lines

--- search_word_in_a_file.py
+++ search_word_in_a_file.py
@@ -1,3 +1,5 @@
+# This project is for a text file with multiple lines
+
 import tkinter as tk
 from tkinter.filedialog import askopenfilename
 import os
@@ -8,6 +10,8 @@ root.title("I wil find u")
 root.geometry('400x100')
 
 file_name = ""
+success = False
+
 def openfile():
     global file_name
     # global keyword is used to reference to the variable outisde this method
@@ -17,9 +21,10 @@ def openfile():
                                 filetypes=(("Text Files", "*.txt*"), ("Python Files", "*.py*")),
                                 # only allows the file dialog to select file with .py and .txt extension
                                 title='Choose a file.')
-                                # sets a title for the file dialog window
+    # sets a title for the file dialog window
     file_name = (os.path.basename(file_path))
     filenameentry.delete(0, 'end')
+    # delete the values inside the entry
     filenameentry.insert(0, file_path)
     # returns the base name i.e file name of the path selected
     # os.path.dirname returns the head whereas os.path.basename returns the tail of the path selected
@@ -31,19 +36,34 @@ def findit(searchword, name):
     elif name == "":
         messagebox.showerror("Error", "Please select a file first")
     else:
-        mylines = []
+        mylines = []  # creating a list
         filehandle = open(name, 'r')
+        print(type(filehandle))
         for lines in filehandle:
             mylines.append(lines.split())
-        # print(mylines)
+            # split statement without any parameter splits the lines on the basis of whitespace
+            # here every line in the text file is splitted on the basis of whitespace
+            # the splitted lines are again being appended(added) to a list and the list can be seen below
+        '''
+        [['Keep', 'your', 'paragraph', 'centered', 'on', 'one', 'idea.'],
+         ['There', 'is', 'nothing', 'more', 'frustrating', 'for', 'readers', 'when', 'they', 'read', 'a', 'paragraph',
+          'that', 'talks', 'about', 'one', 'thing,', 'only', 'to', 'find', 'that', 'it', 'runs', 'off', 'track', 'and',
+          'discusses', 'something', 'else.'],
+         ['For', 'instance,', 'if', 'you', 'are', 'discussing', 'your', 'protagonist,', 'keep', 'the', 'paragraph',
+          'on', 'that', 'character', 'and', 'nothing', 'else.', 'If', 'you', 'were', 'to', 'continue', 'talking',
+          'about', 'the', 'car', 'waiting', 'outside,', 'and', 'then', 'cats,', 'dogs', 'and', 'pineapples', 'in',
+          'the', 'same', 'paragraph,', 'the', 'reader', 'will', 'get', 'confused.'], ['Keep', 'it', 'simple.'],
+         ['One', 'paragraph,', 'one', 'central', 'idea.']]
+        '''
         for line in mylines:
-            # print(wordlist)
-            if searchword in set(line):
-                messagebox.showinfo("Found in the line", line)
-                # print(wordlist)
-            else:
-                messagebox.showinfo("Not Found", "Please search another word")
-                break
+            for i in range(len(line)):
+                global success
+                if line[i] == searchword:
+                    success = True
+                    messagebox.showinfo("Found in this line", line)
+        if not success:
+            messagebox.showinfo("Not Found", "Probably not in the file")
+        filehandle.close()
 
 
 tk.Label(root, text="Simple Search GUI", fg="red", font=('Helvetica', 16)).grid(row=0, column=2)
@@ -57,8 +77,7 @@ filenameentry.grid(row=1, column=2, ipadx=50)
 searchentry.grid(row=2, column=2, ipadx=50)
 
 SelectFile = tk.Button(root, text="Select a File", bg="orange", command=openfile).grid(column=3, row=1)
-Search = tk.Button(root, text="Search", bg="orange", command=lambda: findit(searchentry.get(), file_name))\
+Search = tk.Button(root, text="Search", bg="orange", command=lambda: findit(searchentry.get(), file_name)) \
     .grid(column=3, row=2)
-
 
 root.mainloop()
